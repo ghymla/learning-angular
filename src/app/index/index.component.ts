99d72355ddfe011b3b9a08897d9759696c7041d7
cloteau camille <camille.cloteau@gmail.com>
2022-05-13 16:17:42 +0200

    search objet

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MANGAS } from '../mangas/mangas-list'
+import { Manga } from '../mangas/manga';
 
 @Component({
   selector: 'app-index',
@@ -6,13 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  mangaList: Manga[] = MANGAS;
+  selectManga: Manga|undefined;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  recupInput(value: string){
-    console.log(`la valeur du INPUT ici: ${value}`)
+  mangaSelect(value: string){
+    const manga: Manga|undefined = this.mangaList.find(Manga => Manga.name === value);
+    this.selectManga = manga;
   }
 }
