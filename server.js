28d59fa54850cf6ebb45f58096c11548b49acff1
cloteau camille <camille.cloteau@gmail.com>
2022-10-30 10:46:58 +0100

    bug fix heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/recap-angular'));
+app.use(express.static(__dirname + '/dist/learning-angular'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/recap-angular/index.html'));});
